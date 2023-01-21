7cc26f3fdba007d6ce5bb7cca76bda7a5af68d84
Aksel Telle <54355767+akseltelle@users.noreply.github.com>
2023-01-21 02:17:13 +0100

    Update telegram.sh

--- telegram.sh
+++ telegram.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bot_token="**********************************************" ## You can get a token from https://t.me/BotFather.
 chat_id="**********" ## You can get your chat ID by sending a message to the bot and going to this URL to view the chat_id: https://api.telegram.org/bot**********************************************/getUpdates
-set -- example.com example.net
+set -- example.com example.net raw.githubusercontent.com/akseltelle/Bash-Website-Monitoring/main ## All URL's are accepted, as long as the telegram.php file can be found. https:// and /telegram.php is added to the complete URL automatically, so if the complete URL is https://example.com/telegram.php, only add example.com to this list.
 date="$(date +%d/%m/%Y\ %H:%M:%S)"
 for arg; do
    status="$(wget --timeout=2 --tries=1 -q -O - https://$arg/telegram.php)"
